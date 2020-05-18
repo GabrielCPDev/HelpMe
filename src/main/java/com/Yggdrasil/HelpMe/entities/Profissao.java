5ed3c4fc1e6ea2fc646ad1831e0cdaf7c39509a1
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2020-05-18 18:50:10 -0300

    criando entidade Categoria, mapeando relacao com Profissao e implementando metodos GET , PUT, DELETE E POST E PAGE

--- src/main/java/com/Yggdrasil/HelpMe/entities/Profissao.java
+++ src/main/java/com/Yggdrasil/HelpMe/entities/Profissao.java
@@ -36,6 +36,14 @@ public class Profissao implements Serializable{
 	private List<Trabalhador> trabalhadores = new ArrayList<>();
 	
 	@JsonIgnore
+	@ManyToMany
+	@JoinTable(name = "PROFISSAO_CATEGORIA",
+		joinColumns = @JoinColumn(name = "profissao_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
+		
+	@JsonIgnore
 	@OneToMany(mappedBy = "id.profissao")
 	private Set<ItemPedido> itens = new HashSet<>();
 		
@@ -89,5 +97,38 @@ public class Profissao implements Serializable{
 	public void setItens(Set<ItemPedido> itens) {
 		this.itens = itens;
 	}
+	public List<Categoria> getCategrias() {
+		return categorias;
+	}
+
+	public void setCategrias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Profissao other = (Profissao) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 
 }
---
+++ src/main/java/com/Yggdrasil/HelpMe/services/CategoriaService.java
@@ -0,0 +1,61 @@
+package com.Yggdrasil.HelpMe.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.Yggdrasil.HelpMe.dto.CategoriaDTO;
+import com.Yggdrasil.HelpMe.entities.Categoria;
+import com.Yggdrasil.HelpMe.repositories.CategoriaRepository;
+import com.Yggdrasil.HelpMe.services.exceptions.DataIntegrityExceptionException;
+import com.Yggdrasil.HelpMe.services.exceptions.ObjetoNaoEncontradoException;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar (Integer id) throws Exception {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException("Categoria não encontrada! " + id + " , Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria salvar(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria atualizar(Categoria obj) throws Exception {
+		buscar(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void excluir(Integer id) throws Exception {
+		buscar(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityExceptionException("Não é possível excluir essa Categoria pois existem profissões ligadas a ela!");
+		}
+	}
+	
+	public List<Categoria> listarCategoria(){
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> EncontrarPagina(Integer pagina, Integer linhasPorPagina, String ordenarPor, String direcao){
+		PageRequest pageRequest = PageRequest.of(pagina, linhasPorPagina, Direction.valueOf(direcao), ordenarPor);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+}
